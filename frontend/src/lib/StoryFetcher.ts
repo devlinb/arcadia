5ec37c54d0bf3b489ba70df6fa8f7914d86d53e7
Devlin Bentley <devlin.bentley@gmail.com>
2023-05-03 09:37:42 -0700

    Re-enabled pregenerated stories

--- frontend/src/lib/StoryFetcher.ts
+++ frontend/src/lib/StoryFetcher.ts
@@ -1,7 +1,7 @@
 import type { TRelationship, TPerson, TStorySubmission } from "../../../shared";
 
 export const fetchStory = async (submission: TStorySubmission): Promise<string> => {
-    //if (JSON.stringify(pregenCharacters) === JSON.stringify(submission)) return pregenStories[0].story;
+    if (JSON.stringify(pregenCharacters) === JSON.stringify(submission)) return pregenStories[0].story;
     const url = new URL(`${import.meta.env.VITE_API_SERVER}${import.meta.env.VITE_PROMPT_URL}`);
     url.searchParams.append('kingdom', submission.kingdom);
     url.searchParams.append('people', JSON.stringify(submission.people));
